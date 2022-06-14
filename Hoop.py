5097d35e03bc2a5a93601612f378b433dc164e31
Arman <arman.s.m@outlook.com>
2022-06-14 18:31:29 +0430

    hoop updated with features such as being able to play again in the program and scores list

--- Hoop.py
+++ Hoop.py
@@ -2,28 +2,36 @@ from time import time
 
 print("Hi, welcome to this game named (Hoop)\nYou have 5 seconds for each turn! - Type * instead of hoop!")
 
-lost = False
-cyclesCount = 0
-cycleLength = int(input("How long should each cycle of saying hoop take? "))
+scores = list()
 
-while not lost:    
-    #Computer's guess
-    cyclesCount += 1
-    if cyclesCount % cycleLength == 0:
-        print("Program: *")
-    else:
-        print("Program:", cyclesCount)
+while True:
+    lost = False
+    cyclesCount = 0
+    cycleLength = int(input("How long should each cycle of saying hoop take? "))
+    score = 0
     
-    #Player's guess
-    cyclesCount += 1
-    inputTime = time()
-    playerInput = input("Player: ")
+    while not lost:    
+        #Computer's guess
+        cyclesCount += 1
+        if cyclesCount % cycleLength == 0:
+            print("Program: *")
+        else:
+            print("Program:", cyclesCount, sep=" ")
 
-    if time() - inputTime > 5:
-        lost = True
-    elif playerInput != "*" and cyclesCount % cycleLength == 0:
-        lost = True
-    elif playerInput != str(cyclesCount) and cyclesCount % cycleLength != 0:
-        lost = True
+        #Player's guess
+        cyclesCount += 1
+        inputTime = time()
+        playerInput = input("Player: ")
 
-print("Gameover!")
+        if time() - inputTime > 5 or (playerInput != str(cyclesCount) and cyclesCount % cycleLength != 0) or playerInput != "*" and cyclesCount % cycleLength == 0:
+            scores.append(score)
+            lost = True
+        else:
+            score += 1
+
+    print("You lost!\n", "Scores:", "-" * 10, sep="\n")
+    for idx, score in enumerate(sorted(scores, reverse=True)):
+        print(f"<{idx + 1}>: {score}", "⭐" if idx == 0 else "", sep=" ")
+
+    if input("Would you like to play again (y/n)?").lower() != "y":
+        exit()
