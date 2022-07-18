478a739febe1305f7086065c0280ebe2066e4ed2
arman salimi <82840553+Arman-sm@users.noreply.github.com>
2022-07-18 20:19:29 +0430

    Change GameHub command system
    In this update the command system completely changed and from now the bakeCommand function is going to take care of most of the work for processing the commands such as detecting and organizing the tags

--- GameHub.py
+++ GameHub.py
@@ -2,40 +2,60 @@ from importlib import *
 from os import *
 
 games = {}
+commands = ["detectDir", "clear", "quit"]
 
-def deconstruct(value):
-    chunks = []
-    for chunk in value.split():
+
+#Organizing the command in a dict
+def bakeCommand(command):
+    #Separating commands and tags for organizing them
+    commandChunks = []
+    for chunk in command.split():
         if chunk != "":
-            chunks.append(chunk)
-    return chunks
+            commandChunks.append(chunk)
+    #Checking if the command is available in the games or in the main commands list
+    if commandChunks[0] in commands or commandChunks[0] in games:    
+        #Adding the main command name to the command info
+        command = {"command" : commandChunks[0]}
+        #Adding the tags to the command info
+        for index, tag in enumerate(commandChunks[1:]):
+            if tag[0] == "-":
+                command.update({tag : (commandChunks[index+2] if commandChunks[index+2][0] != "-" else True) if len(commandChunks) >= index + 2 else True})
+        if command["command"] in commands or command["command"] in games:
+            return command
+    else:
+        return "Command not found"
+#Commands
+def clear(_):
+    system("cls")
 
-def autoDetectGames(filePath = "."):
-    if filePath != "." and not path.isdir(filePath):
-        print("Directory not found.")
+def quit(_):
+    exit()
+
+def detectDir(command):
+    if not "-p" in command:
+        command.update({"-p" : "."})
+    elif command["-p"] != "." and not path.isdir(command["-p"]):
+        print("Directory not found")
         
-    validFilesList = []
-    for file in listdir(filePath):
+    validFiles = []
+    for file in listdir(command["-p"]):
         if path.splitext(file)[1] == ".py":
-            validFilesList.append(path.splitext(file)[0])
+            validFiles.append(path.splitext(file)[0])
     global games
-    if input("Are you sure? They might cause malicious activities on your computer. (y/n): ").lower() == "y":
-        for game in validFilesList:
-            games.update({game : path.join(filePath if filePath != '.' else '', game)})
-            print(games.items())
+    if input("Are you sure? They might cause malicious activities on your computer! (y/n): ").lower() == "y":
+        for game in validFiles:
+            if game != path.splitext(path.basename(__file__))[0]:
+                games.update({game : path.join(command["-p"] if command["-p"] != '.' else '', game)})
+                print(games.items())
 
+#Main loop
 while(True):
-    command = input("Command: ")
-    if "autoDetect" in command:
-        if "-p" in command:
-            command = deconstruct(command)
-            autoDetectGames(command[command.index("-p")+1])
-        else:
-            autoDetectGames()
-    elif command in games:
-        game = __import__(games[command])
-        game.main()
-    elif command == "exit" :
-        exit()
-    elif command in ["clear", "cls"]:
-        system("cls")
+    command = bakeCommand(input("Command: "))
+    if type(command) == dict:
+        if command["command"] in globals():
+            globals()[command["command"]](command)
+        elif games[command["command"]] in games:
+            game = __import__(games[command["command"]])
+            game.main()
+    else:
+        print(command)
